7995cf445c62bf9dd484115b8676539735cc9c95
dmidd87 <dmidd87@gmail.com>
2014-08-11 16:11:00 -0600

    Not sure if parent/child deletion
    Trying to delete individual items off my list

--- js/app.js
+++ js/app.js
@@ -3,11 +3,17 @@ $(document).ready(function() {
     $("#add").click(function() {
         var inputValue = $("#input").val();
         
-        $("list").append("<p>"+inputValue+"<a class='delete'> X </a></p>");
+        $("#list").prepend("<p>"+inputValue+"<a class='delete'> X </a></p>");
+        
+            
     });
     
     
+    $(".delete").click(function() {
+            $(".delete").remove("+inputValue+");
+    });
+        
+    
 });
-
 
     
